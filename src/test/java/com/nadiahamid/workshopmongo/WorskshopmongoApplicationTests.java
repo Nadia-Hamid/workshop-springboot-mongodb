40bece1489fc3870a1a1b15de4a7358260cb8346
Nadia-Hamid <hamid.nadia@gmail.com>
2020-07-02 10:35:32 +0200

    primeiro commit- projeto criado

---
+++ src/test/java/com/nadiahamid/workshopmongo/WorskshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadiahamid.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorskshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
