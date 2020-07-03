1e5af3c435b5e98d2748d7948c1b8529e63d02fb
Nadia-Hamid <hamid.nadia@gmail.com>
2020-07-03 09:24:12 +0200

    Delecao de usuario com DELETE

--- src/main/java/com/nadiahamid/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nadiahamid/workshopmongo/resources/UserResource.java
@@ -32,10 +32,10 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		User obj = service.findById(id);
-		return ResponseEntity.ok().body(new UserDTO(obj));
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
