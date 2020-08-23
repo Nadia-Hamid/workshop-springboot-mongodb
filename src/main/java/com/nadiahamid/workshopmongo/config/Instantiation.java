1ac4a09ad7a3843e150088878d82c7fd9a9c5e7a
Nadia-Hamid <hamid.nadia@gmail.com>
2020-08-23 14:00:52 +0200

    Referenciando os Posts do usuário

--- src/main/java/com/nadiahamid/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nadiahamid/workshopmongo/config/Instantiation.java
@@ -39,9 +39,14 @@ public class Instantiation implements CommandLineRunner {
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abracos",
 				new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia!", "Acordei feliz hoje!",
+				new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 
 }
